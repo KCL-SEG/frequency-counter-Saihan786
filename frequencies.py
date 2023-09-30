c42a910dd9ce63642b283649771a6759b8a06619
Saihan Marshall <saihan.marshall@kcl.ac.uk>
2023-09-30 07:01:07 +0100

    Added functionality to "frequencies.py". It now finds the frequencies of each item which is converted to a string.

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,18 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+items = ['0', 4,4,'4','d','d','e',0,'a','d','4']
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+
+    for item in items:
+        item = str(item)
+        
+        if item in frequencies:
+            frequencies[item] = frequencies[item]+1
+        else:
+            frequencies[item] = 1
     return frequencies
+
+print(frequencies(items))
